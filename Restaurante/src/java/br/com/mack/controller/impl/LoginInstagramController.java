9baca22f5434789e9a390158b769c61e74e2df12
31595472 <31595472@087895-P33S104.macklabs.br>
2017-05-09 11:59:09 -0300

    Início do login com Instagram.

---
+++ Restaurante/src/java/br/com/mack/controller/impl/LoginInstagramController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mack.controller.impl;
+
+import br.com.mack.controller.AbstractController;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+
+/**
+ *
+ * @author 31595472
+ */
+public class LoginInstagramController extends AbstractController{
+
+    @Override
+    public void execute() {
+        
+        
+        
+    }
+    
+}
